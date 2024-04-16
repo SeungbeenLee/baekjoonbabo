5ac4b0d671ad188c643c0968261dfd7faad9f5bb
seungbel <seungbel@student.42.fr>
2024-04-16 12:15:11 +0900

    day27_backtracking_last

--- bj1759.cpp
+++ bj1759.cpp
@@ -2,9 +2,35 @@
 #include <algorithm>
 using namespace std;
 
-int n, m;
-char arr[20];
-int per[20];
+int		n, m;
+char	ans[20];
+char	arr[20];
+int		isused[20];
+
+void func(int k, int before, int cons, int vowel)
+{
+	if (k == n)
+	{
+		if (cons < 2 || vowel < 1)
+			return ;
+		for (int i=0;i<n;i++) cout << ans[i];
+		cout << '\n';
+		return ;
+	}
+	for (int i=before;i<m;i++)
+	{
+		if (!isused[i])
+		{
+			ans[k] = arr[i];
+			isused[i] = 1;
+			if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u')
+				func(k+1, i, cons, vowel+1);
+			else
+				func(k+1, i, cons+1, vowel);
+			isused[i] = 0;
+		}
+	}
+}
 
 int main(void)
 {
@@ -14,12 +40,5 @@ int main(void)
 	cin >> n >> m;
 	for (int i=0;i<m;i++) cin >> arr[i];
 	sort(arr, arr+m);
-
-	for (int i=0;i<n;i++) per[i] = 1;
-	do
-	{
-		for (int i=0;i<m;i++)
-			if (per[i] == 1) cout << arr[i];
-		cout << '\n';
-	} while (prev_permutation(per,per+m));
+	func(0, 0, 0, 0);
 }
